acbf69b3245195f1927beadd63af9ade001c47af
Kacper Pietrzak <kacpietrzak@gmail.com>
2018-08-19 11:47:19 -0400

    Refactor, merge definitions with declarations in rpcserver for clarity.

--- backend/rpcServer/rpcserver.h
+++ backend/rpcServer/rpcserver.h
@@ -5,21 +5,20 @@
 class RPCServer : public AbstractRPCServer
 {
 public:
-   RPCServer(jsonrpc::AbstractServerConnector &connector);
-   void sayHello();
+   RPCServer(jsonrpc::AbstractServerConnector &connector)
+        :AbstractRPCServer(connector),
+        didSayHello(false)
+    {
+    }
+
+    void sayHello()
+    {
+        std::cout << "BACKEND: Hello!\n";
+        didSayHello = true;
+    }
 
    bool didSayHello;
 };
 
-RPCServer::RPCServer(jsonrpc::AbstractServerConnector &connector)
-   :AbstractRPCServer(connector),
-    didSayHello(false)
-{
-}
 
-void RPCServer::sayHello()
-{
-   std::cout << "BACKEND: Hello!\n";
-   didSayHello = true;
-}
 
